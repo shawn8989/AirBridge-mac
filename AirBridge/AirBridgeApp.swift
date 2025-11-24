58d16d8468a8f2cc7f45370113be3efe7dc2274d
shawn8989 <shunathon@icloud.com>
2025-11-24 14:02:37 -0800

    Initial Commit

---
+++ AirBridge/AirBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBridgeApp.swift
+//  AirBridge
+//
+//  Created by shunathon Owens on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AirBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBridgeTests/AirBridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirBridgeTests.swift
+//  AirBridgeTests
+//
+//  Created by shunathon Owens on 11/24/25.
+//
+
+import Testing
+@testable import AirBridge
+
+struct AirBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
